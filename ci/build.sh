e7bd56be0317d949e80b9480107715cbcc9376fb
Adam Woods <adam.tomato@gmail.com>
2018-07-20 00:06:42 +1000

    enable build

--- ci/build.sh
+++ ci/build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env sh
 
 export ROOT_FOLDER=$( pwd )
-GRADLE_HOME="${HOME}/.gradle"
-GRADLE_CACHE="${ROOT_FOLDER}/gradle"
-[[ -d "${GRADLE_CACHE}" && ! -d "${GRADLE_HOME}" ]] && ln -s "${GRADLE_CACHE}" "${GRADLE_HOME}"
+export GRADLE_USER_HOME="${ROOT_FOLDER}/.gradle"
 
 cd nug
 ./gradlew --no-daemon build
